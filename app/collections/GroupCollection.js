963958c41480052434f124d73bbc58167ad46cdf
Vasyl Priymachuk <activeobject.ac@gmail.com>
2013-06-22 14:57:38 +0300

    Fetch group info from vk.api instead own service

--- app/collections/GroupCollection.js
+++ app/collections/GroupCollection.js
@@ -1,27 +1,32 @@
 var BaseCollection = require('models/supers/base').Collection;
 var Group = require('models/Group');
 var app = require('app');
-
-var embedded = function (resource) {
-	return resource._embedded;
-};
+var handleError = require('lib/common').handleError;
 
 module.exports = Backbone.Collection.extend({
 	model: Group,
 	path: 'groups',
 	parse: function (res) {
-		return _.values(embedded(res));
+		var count = res[0];
+		return res.slice(1);
 	},
 	sync: function (method, model, options) {
-		var path = _.isFunction(this.path) ? this.path() : this.path;
-		app.api.resource(path, function (err, resource, status, xhr) {
-			if (err) {
-				if (options.error) options.error(model, xhr, options);
-				return model.trigger('error', model);
-			}
+		var onError = function (err) {
+			if (options.error) options.error(model, err, options);
+			model.trigger('error', model);
+		};
 
-			if (options.success) options.success(resource, status, xhr);
-			model.trigger('sync', model, resource, options);
-		});
+		var onLoad = function (res, status, xhr) {
+			if (options.success) options.success(res, status, xhr);
+			model.trigger('sync', model, res, options);
+		};
+
+		// TODO: handle case where user has more than 100 groups
+		app.vk.groups.get({
+			user_id: app.vk.user,
+			extended: 1,
+			offset: 0,
+			count: 100
+		}, handleError(onLoad, onError));
 	}
 });
