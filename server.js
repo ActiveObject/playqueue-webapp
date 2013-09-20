0ce812586e0fb89cc6d76b6ae32d74fd517ea370
Vasyl Priymachuk <vasyl.priymachuk@sethq.com>
2013-09-20 12:58:02 +0300

    fix bug with missing files

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ exports.startServer = function (port, publicPath, callback) {
   var fileServer = new(static.Server)(publicPath);
   require('http').createServer(function (req, res) {
     req.addListener('end', function () {
-      fileServer.serve(req, res, function (err, res) {
+      fileServer.serve(req, res, function (err) {
         if (err && (err.status === 404)) {
           fileServer.serveFile('index.html', 200, {}, req, res);
         }
